a3ab86a5e45686016640fada0d461460d57ec268
Anubhav Ishan <anubhavishan90a1@gmail.com>
2025-07-02 15:45:19 +0530

    Initial commit

--- chatbot/urls.py
+++
@@ -1,8 +0,0 @@
-# chatbot/urls.py
-from django.urls import path
-from .views import forex_chatbot, chat_page
-
-urlpatterns = [
-    path('chat/', forex_chatbot, name='forex_chatbot'),
-    path('', chat_page, name='chat_page'),
-]
